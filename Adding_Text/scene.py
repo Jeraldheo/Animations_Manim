05e4d21d8eb79c492bce0d0a09d5c97ff453fd97
Jeraldheo <jeyson.marin@unah.hn>
2022-08-14 00:49:40 -0600

    Finished firs example, working on the second.

--- Adding_Text/scene.py
+++ Adding_Text/scene.py
@@ -1,4 +1,5 @@
 
+
 from manim import *
 
 class Node(VGroup):
@@ -30,10 +31,15 @@ class NArrow(Arrow):
         radius_n1=node_1.get_radius()
         radius_n2=node_2.get_radius()
         reference_line=Line(node_1.get_center(),node_2.get_center())
-        start_point=node_1.get_center()+reference_line.get_unit_vector()*radius_n1
-        end_point=node_2.get_center()-reference_line.get_unit_vector()*radius_n2
-        super().__init__(start_point,end_point,**kwargs)
+        self.start_point=node_1.get_center()+reference_line.get_unit_vector()*radius_n1
+        self.end_point=node_2.get_center()-reference_line.get_unit_vector()*radius_n2
+        super().__init__(self.start_point,self.end_point,**kwargs)
 
+    def get_start(self):
+        return self.start_point
+
+    def get_end(self):
+        return self.end_point
         
 class HelloWorld(Scene):
     def construct(self):
@@ -45,46 +51,52 @@ class HelloWorld(Scene):
         self.play(Write(subtitle)) 
         self.play(FadeOut(subtitle))
 
-        coord_node_1=[-3,3,0]
-        coord_node_2=[0,2,0]
-        coord_node_3=[2,0,0]
-        coord_node_4=[1,-3,0]
-        coord_node_5=[-2,-2,0]
+        example1 = Table([['1','3','4','2','2'],], include_outer_lines=True)
+        self.play(Create(example1))
+
+        indices = [Text(str(i)) for i in range(5)]
+
+        indices = VGroup(indices[0], indices[1], indices[2], indices[3], indices[4]).arrange(5*RIGHT)
+        indices.next_to(example1, DOWN, buff=0.5)
+        self.play(Create(indices))
+
+        self.play(FadeOut(example1))
+        self.play(FadeOut(indices))
+
         
+        coord_nodes = [[-3,3,0], [0,2,0],[2,0,0], [1,-3,0], [-2,-2,0]]
+        n = len(coord_nodes)
+        nodes = [Node(str(i), coord_nodes[i]) for i in range(n)]
 
-        node_1=Node("0",coord_node_1)
-        node_2=Node("1",coord_node_2)
-        node_3=Node("2",coord_node_3)
-        node_4=Node("3",coord_node_4)
-        node_5=Node("4",coord_node_5)
-
-        edge1=NArrow(node_1,node_2)
-        edge2=NArrow(node_2,node_3)
-        edge3=NArrow(node_3,node_4)
+        edges =[NArrow(nodes[i], nodes[i+1]) for i in range(n-1)]
+        edges.append(CurvedArrow(edges[3].get_start(), edges[3].get_end()))
+        edges.append(CurvedArrow(edges[3].get_end(), edges[3].get_start()))
         
+        for i in range(n):
+            self.play(Create(nodes[i]))
+
+        for i in range(n+1):
+            if i!=3:
+             self.play(Create(edges[i]))
+
+        for i in range(n):
+            self.play(FadeOut(nodes[i]), run_time=0.1)
+
+        for i in range(n+1):
+            if i!=3:
+             self.play(FadeOut(edges[i]), run_time=0.1)
+
+
         
+        example2 = Table([['3','4','1','5','2', '4'],], include_outer_lines=True)
+        self.play(Create(example2))
 
+        indices2 = [Text(str(i)) for i in range(6)]
 
-        radius_n5=node_5.get_radius()
-        radius_n4=node_4.get_radius()
-        reference_line=Line(node_5.get_center(),node_4.get_center())
-        start_point=node_5.get_center()+reference_line.get_unit_vector()*radius_n5
-        end_point=node_4.get_center()-reference_line.get_unit_vector()*radius_n4
-        last_edge = CurvedArrow(start_point, end_point)
-        edge4 = CurvedArrow(end_point, start_point)
+        indices2 = VGroup(indices2[0], indices2[1], indices2[2], indices2[3], indices2[4], indices2[5]).arrange(5*RIGHT)
+        indices2.next_to(example2, DOWN, buff=0.5)
+        self.play(Create(indices2))
 
-        self.play(Create(node_1))
-        self.play(Create(node_2))
-        self.play(Create(node_3))
-        self.play(Create(node_4))
-        self.play(Create(node_5))
-        self.play(Create(edge1))
-        self.play(Create(edge2))
-        self.play(Create(edge3))
-        self.play(Create(edge4))
-        self.play(Create(last_edge))
-
-
-
-        self.wait(duration=3)
+        self.play(FadeOut(example2))
+        self.play(FadeOut(indices2))
        
